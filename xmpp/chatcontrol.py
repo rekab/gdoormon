ed9b911774d1f41838cdea71ea13fcdd0339e306
james <james@havidol>
2013-09-25 22:13:26 -0700

    xmpp chatcontrol tests pass

--- xmpp/chatcontrol.py
+++ xmpp/chatcontrol.py
@@ -10,7 +10,6 @@ from wokkel import subprotocols
 
 
 class SendMessageMixin:
-  """oh god how did this get here i am not good with oop"""
   def sendMessage(self, body, to): 
     msg = domish.Element((None, 'message'))
     msg['to'] = to
@@ -19,17 +18,17 @@ class SendMessageMixin:
     self.send(msg)
 
 
-class ChatBroadcastProtocol(SendMessageMixin, XMPPHandler):
+class ChatBroadcastProtocol(SendMessageMixin, xmppim.XMPPHandler):
   def __init__(self, subscribers):
     self.subscribers = subscribers
 
   def sendAllSubscribers(self, text):
-    for subscriber in self.getAllSubscribers():
+    for subscriber in self.subscribers:
       self.sendMessage(text, subscriber)
 
 
 class ChatCommandReceiverProtocol(SendMessageMixin, xmppim.MessageProtocol):
-  command_re = re.compile(r'(?P<command>\w+)\s*(?P<args>.+)')
+  command_re = re.compile(r'(?P<command>\w+)\s*(?P<args>.*)')
 
   def __init__(self, statemach, subscribers):
     self.statemach = statemach
@@ -43,10 +42,21 @@ class ChatCommandReceiverProtocol(SendMessageMixin, xmppim.MessageProtocol):
     if not (msg["type"] == 'chat' and hasattr(msg, "body") and msg.body):
       return
     command_match = self.command_re.match(msg.body)
-    result = self.dispatchCommand(
-        msg["from"], command_match.group(1),
-        re.split('\s+', comand_match.group(2)))
+    if command_match:
+      result = self.dispatchCommand(
+          msg["from"], command_match.group(1),
+          re.split('\s+', command_match.group(2)))
+    else:
+      result = 'what?'
     self.sendMessage(result, msg["from"]) 
+
+  def dispatchCommand(self, sender, cmd_str, cmd_args):
+    event_name = 'command_%s' % cmd_str.lower()
+    if hasattr(self, event_name):
+      return getattr(self, event_name)(sender, cmd_args)
+    if self.statemach.can(event_name):
+      return getattr(self.statemach, event_name)(sender, cmd_args)
+    return "bad command"
 
   def command_subscribe(self, sender, cmd_args):
     self.subscribers.add(sender)
@@ -58,7 +68,7 @@ class ChatCommandReceiverProtocol(SendMessageMixin, xmppim.MessageProtocol):
       return '%s unsubscribed' % sender
     return '%s not subscribed' % sender
 
-  def command_snooze(self, cmd_args):
+  def command_snooze(self, sender, cmd_args):
     duration = None
     if not cmd_args:
       return 'need a duration'
@@ -68,13 +78,5 @@ class ChatCommandReceiverProtocol(SendMessageMixin, xmppim.MessageProtocol):
       return 'cannot parse "%s"' % cmd_args[0]
     self.statemach.snoozeAlert(int(cmd_args))
 
-  def command_help(self, cmd_args):
+  def command_help(self, sender, cmd_args):
     return "help yourself"
-
-  def dispatchCommand(self, sender, cmd_str, cmd_args):
-    event_name = 'command_%s' % cmd_str.lower()
-    if getattr(self, event_name):
-      return getattr(self, event_name)(sender, cmd_args)
-    if self.statemach.can(event_name):
-      return getattr(self.statemach, event_name)(sender, cmd_args)
-    return "bad command"
---
+++ xmpp/test_chatcontrol.py
@@ -0,0 +1,114 @@
+from twisted.trial import unittest
+from twisted.words.xish import domish
+from xmpp import chatcontrol
+
+
+class FakeStatemach(object):
+  def __init__(self, valid_commands):
+    self.valid_commands = valid_commands
+    self.called = []
+
+  def can(self, command):
+    return command in self.valid_commands
+
+  def __getattr__(self, attr):
+    return lambda sender, args: self.recordCall(attr, sender, args)
+
+  def recordCall(self, attr, sender, args):
+    self.called.append((attr, sender, args))
+    return 'ok'
+
+
+class TestChatProtocolBase(unittest.TestCase):
+  def setUp(self):
+    self.sent = []
+    self.subscribers = set(['a', 'b', 'c'])
+
+  def mockSend(self, msg):
+    self.sent.append(msg)
+
+
+class TestBroadcastProtocol(TestChatProtocolBase):
+  def setUp(self):
+    TestChatProtocolBase.setUp(self)
+    self.broadcast = chatcontrol.ChatBroadcastProtocol(self.subscribers)
+    self.patch(self.broadcast, 'send', self.mockSend)
+
+  def testSendAllSubscribers(self):
+    self.broadcast.sendAllSubscribers('hello')
+    self.assertNotEquals(len(self.subscribers), 0)
+    for sent in self.sent:
+      self.subscribers.remove(sent['to'])
+    self.assertEquals(len(self.subscribers), 0)
+
+
+class ChatCommandReceiverProtocol(TestChatProtocolBase):
+  def setUp(self):
+    TestChatProtocolBase.setUp(self)
+    self.statemach = FakeStatemach(set(['command_test_command']))
+    self.receiver = chatcontrol.ChatCommandReceiverProtocol(
+        self.statemach, self.subscribers)
+    self.patch(self.receiver, 'send', self.mockSend)
+
+  def receiveFakeMessage(self, text, sender):
+    msg = domish.Element((None, 'message'))
+    msg['from'] = sender
+    msg['type'] = 'chat'
+    msg.body = text
+    self.receiver.onMessage(msg)
+
+  def testHelp(self):
+    self.receiveFakeMessage('help me!', 'foo')
+    self.assertEquals(1, len(self.sent))
+    self.assertEquals('help yourself', str(self.sent[0].body))
+    # Verify the statemach wasn't touched
+    self.assertEquals(0, len(self.statemach.called))
+
+  def testSubscribe(self):
+    # unsubscribe
+    self.receiveFakeMessage('unsubscribe', 'so-and-so')
+    self.assertEquals('so-and-so not subscribed', str(self.sent.pop().body))
+    self.assertTrue('so-and-so' not in self.subscribers)
+
+    # subscribe
+    self.receiveFakeMessage('subscribe', 'so-and-so')
+    self.assertEquals('so-and-so subscribed', str(self.sent.pop().body))
+    self.assertTrue('so-and-so' in self.subscribers)
+
+    # unsubscribe
+    self.receiveFakeMessage('unsubscribe', 'so-and-so')
+    self.assertEquals('so-and-so unsubscribed', str(self.sent.pop().body))
+    self.assertTrue('so-and-so' not in self.subscribers)
+    self.assertEquals(0, len(self.sent))
+
+    # Verify the statemach wasn't touched
+    self.assertEquals(0, len(self.statemach.called))
+
+  def testStateMachCall(self):
+    self.assertEquals(0, len(self.statemach.called))
+    self.receiveFakeMessage('test_command with args', 'foo')
+    self.assertEquals(
+        ('command_test_command', 'foo', ['with', 'args']),
+        self.statemach.called.pop())
+    self.assertEquals(0, len(self.statemach.called))
+
+  def testInvalidCommand(self):
+    self.receiveFakeMessage('hey are you there?', 'foo')
+    self.assertEquals(0, len(self.statemach.called))
+    self.assertEquals('so-and-so unsubscribed', str(self.sent.pop().body))
+    self.assertTrue('so-and-so' not in self.subscribers)
+    self.assertEquals(0, len(self.sent))
+
+  def testStateMachCall(self):
+    self.assertEquals(0, len(self.statemach.called))
+    self.receiveFakeMessage('test_command with args', 'foo')
+    self.assertEquals(
+        ('command_test_command', 'foo', ['with', 'args']),
+        self.statemach.called.pop())
+    self.assertEquals(0, len(self.statemach.called))
+
+  def testInvalidCommand(self):
+    self.receiveFakeMessage('hey are you there?', 'foo')
+    self.assertEquals(0, len(self.statemach.called))
+    self.assertEquals('bad command', str(self.sent.pop().body))
+    self.assertEquals(0, len(self.sent))
