c77a60fe96d451728e107689d9c29c176722f159
james <james@havidol>
2013-11-11 23:07:19 -0800

    add arduino_client and its regtest; clean up other stuff

---
+++ doorsensor/arduino_client.py
@@ -0,0 +1,87 @@
+"""
+Polls the Arduino sensor server for the status of the door.
+
+toggle = arduino_client.StatemachToggle(statemach)
+monitor = arduino_client.DoorSensor(toggle, hostname, port, threshold)
+pressence_service = internet.TimerService(15, monitor.check)
+"""
+
+ARDUINO_HOSTNAME = 'arduino-gdoor'
+ARDUINO_PORT = 80
+THRESHOLD_CM = 10   # anything over 10cm is "open"
+REQUEST_PATH = '/'
+
+from twisted.internet import reactor
+from twisted.python import log
+from twisted.web import client
+from twisted.web import http
+from twisted.internet import protocol
+
+
+class DoorSensor(object):
+  def __init__(self, toggle, hostname=ARDUINO_HOSTNAME, port=ARDUINO_PORT,
+      threshold=THRESHOLD_CM, requestPath=REQUEST_PATH):
+    self.toggle = toggle
+    self.hostname = hostname
+    self.port = port
+    self.threshold = threshold
+    self.requestPath = requestPath
+
+  def check(self):
+    """Check if the door is open by making an HTTP request to the Arduino.
+
+    Args:
+      hostname: host to connect to
+      port: port to connect to
+      threshold: distance to the door
+
+    Returns:
+      boolean - door is open or not
+    """
+    log.msg('Polling %s for distance.' % self.hostname)
+    agent = client.Agent(reactor)
+    d = agent.request('GET', 'http://%s%s' % (self.hostname, self.requestPath));
+    d.addCallback(self._handleResponse)
+    d.addErrback(self._handleError)
+
+  def _handleResponse(self, response):
+    response.deliverBody(DoorMeasurementProtocol(self.toggle, self.threshold))
+
+  def _handleError(self, response):
+    log.msg('Got: %s' % response)
+
+
+class DoorMeasurementProtocol(protocol.Protocol):
+
+  def __init__(self, toggle, threshold):
+    self.toggle = toggle
+    self.threshold = threshold
+
+  def dataReceived(self, data):
+    log.msg('Read from server: %s' % repr(data))
+    try:
+      distance = int(data)
+      self.toggle(distance > self.threshold)
+    except ValueError as e:
+      log.msg('Bad data from the server: %s' % repr(data))
+
+  def connectionLost(self, reason):
+    if not isinstance(reason, client.ResponseDone):
+      if reason.check(http.PotentialDataLoss):
+        # Since the server is an HTTP 1.0 server, we always ignore
+        # http.PotentialDataLoss
+        pass
+      else:
+        # TODO: make this a state change
+        log.msg('failure: %s' % reason)
+
+
+class StatemachToggle(object):
+  def __init__(self, statemach):
+    self.statemach = statemach
+
+  def __call__(self, doorOpen):
+    if doorOpen:
+      self.statemach.door_open()
+    else:
+      self.statemach.door_closed()
--- testing/arduino_client_regtest.py
+++ testing/arduino_client_regtest.py
@@ -1,30 +1,26 @@
 from doorcontrol import maestro
-from presence import airport_clientmonitor
-from presence import clientdb
+from doorsensor import arduino_client
 from state import statemach
-from testing import fake_chatcontrol
-from testing import fake_doorcontrol
+
 from twisted.application import internet
 from twisted.application import service
+from testing import fake_chatcontrol
+from testing import fake_doorcontrol
 from twisted.python import log
-
 
 def fakeOsSystem(command):
   log.msg('os.system("%s")' % command)
 
 
-application = service.Application("gdoormon")
+application = service.Application("arduino_client_regtest")
 
 fake_broadcaster = fake_chatcontrol.FakeChatBroadcastProtocol()
 door_controller = maestro.DoorControl(fake_doorcontrol.FakePololuMicroMaestro())
 sm = statemach.StateMachine(fake_broadcaster, door_controller,
     doorOpenTimeoutSecs=2.1, alertTimeoutSecs=1.3, system=fakeOsSystem)
 
-toggle = airport_clientmonitor.StatemachToggle(sm)
-clients = clientdb.getDb()
-# TODO: config
-airport_hostname = "hoth"
-monitor = airport_clientmonitor.PresenceMonitor(airport_hostname, clients, toggle)
+toggle = arduino_client.StatemachToggle(sm)
+sensor = arduino_client.DoorSensor(toggle)
 
-presence_service = internet.TimerService(15, monitor.check)
-presence_service.setServiceParent(application)
+sensor_service = internet.TimerService(3, sensor.check)
+sensor_service.setServiceParent(application)
