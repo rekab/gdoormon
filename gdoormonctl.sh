f464842a38a709b268f03b5e300bd6fba03ddadf
james <james@havidol>
2013-11-20 22:45:01 -0800

    change start script to an init.d flavored script

---
+++ gdoormonctl.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+PIDFILE=/tmp/gdoormon.pid
+set -e
+dir=$(dirname $0)
+cd $dir
+if [ ! -d deps ] ; then
+  echo '"deps" directory missing: check virtualenv setup' >2
+  exit 1
+fi
+. deps/bin/activate
+
+function is_running() {
+  [ -f $PIDFILE ] && kill -0 $(cat $PIDFILE)
+}
+
+COMMAND="$1"
+shift
+FLAGS=$*
+
+case $COMMAND in
+status)
+  if is_running ; then
+    echo "process is alive as pid " $(cat PIDFILE)
+  else
+    echo "process is dead"
+  fi
+  ;;
+start)
+  if is_running ; then
+    echo "process is already running"
+  else
+    deps/bin/twistd --pidfile=$PIDFILE -y $FLAGS $dir/main.py
+  fi
+  ;;
+stop)
+  if is_running ; then
+    set -x
+    kill $(cat PIDFILE)
+  else
+    echo "process is not running"
+    exit 1
+  fi
+  ;;
+esac
