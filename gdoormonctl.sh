00f2f60cb817050a78f6d52c5aa6111815280d4c
james <james@havidol>
2013-11-26 23:22:28 -0800

    distinguish between dead and not running

--- gdoormonctl.sh
+++ gdoormonctl.sh
@@ -12,7 +12,15 @@ fi
 . deps/bin/activate
 
 function is_running() {
-  [ -f $PIDFILE ] && kill -0 $(cat $PIDFILE)
+  if [ -f $PIDFILE ] ; then 
+    if kill -0 $(cat $PIDFILE) ; then
+      return 0
+    else
+      echo "pidfile $PIDFILE exists but process is not alive" 1>&2
+      return 1
+    fi
+  fi
+  return 1
 }
 
 COMMAND="$1"
@@ -22,9 +30,10 @@ FLAGS=$*
 case $COMMAND in
 status)
   if is_running ; then
-    echo "process is alive as pid " $(cat $PIDFILE)
+    echo -n "process is alive as pid "
+    cat $PIDFILE
   else
-    echo "process is dead"
+    echo "process is not running"
   fi
   ;;
 start)
